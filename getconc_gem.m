bd69f42a27f2ff42cd442bd24a86890727f70dec
ss-91 <40184532+ss-91@users.noreply.github.com>
2023-03-20 10:14:27 -0400

    Update getconc_gem.m

---
+++ getconc_gem.m
@@ -0,0 +1,6 @@
+function [concentration] = getconc_gem(peak_area)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+concentration = (peak_area - 9.407e4) / 4.429e4
+
+end
